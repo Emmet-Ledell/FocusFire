48926dd9b21d231d456f75cc52ab23112a161f77
Emmet-Ledell <emmet.ledell@gmail.com>
2025-11-12 12:54:41 -0500

    initial keyboard and mouse testing

--- mouse_watcher.py
+++ mouse_watcher.py
@@ -3,6 +3,8 @@ from pynput import mouse
 def on_move(x, y):
     print('Pointer moved to {0}'.format(
         (x, y)))
+    if (x == 0.0 and y == 0.0):
+        return False
 
 def on_click(x, y, button, pressed):
     print('{0} at {1}'.format(
